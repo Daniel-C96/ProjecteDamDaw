4ef33f25a1aeeb0d82e1349f27d1a4bf1b2278fc
Jonathan12V <2023_jonathan.valle@iticbcn.cat>
2023-10-20 14:36:03 +0200

    Archivo Application ruta repository arreglado

--- src/main/java/com/example/demo/ProjecteDamDawApplication.java
+++ src/main/java/com/example/demo/ProjecteDamDawApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.example.demo.repositoy")
+@EnableJpaRepositories("com.example.demo.repository")
 @EntityScan("com.example.demo.model")
 @SpringBootApplication
 public class ProjecteDamDawApplication {
@@ -13,5 +13,4 @@ public class ProjecteDamDawApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjecteDamDawApplication.class, args);
 	}
-
 }
